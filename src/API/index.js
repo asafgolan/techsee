31f7befb0bc1393bd8b25ca58c8edf0d8d0544b8
asafgolan <smartush78@gmail.com>
2020-03-20 18:04:01 +0200

    'api-call'

--- src/API/index.js
+++ src/API/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const testerURL = 'https://cors-anywhere.herokuapp.com/https://test-api.techsee.me/api/ex/'
+const url = 'https://cors-anywhere.herokuapp.com/https://test-api.techsee.me/api/ex/'
 
-function handleError (error) {
-  console.warn(error);
-  return null;
+export default function getTesters(name){
+  return axios.get(url + name )
+      .then(({data}) => data)
+      .catch(errorHandler)
 }
 
-export default function getTesters(tester){
-  return axios.get(testerURL + tester )
-      .then(({data}) => data)
-      .catch(handleError)
+function errorHandler (error) {
+  console.warn(error);
+  return null;
 }
